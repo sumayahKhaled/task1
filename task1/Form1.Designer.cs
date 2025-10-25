c14d69c89de7d416e921b865b9f2315aa71e2bca
sumayahKhaled <sumayahalyosufy@gmail.com>
2025-10-25 19:37:22 +0300

    Add project files.

---
+++ task1/Form1.Designer.cs
@@ -0,0 +1,365 @@
+﻿namespace task1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblBirthdate = new System.Windows.Forms.Label();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.rdoFemale = new System.Windows.Forms.RadioButton();
+            this.rdoMale = new System.Windows.Forms.RadioButton();
+            this.cmbCountry = new System.Windows.Forms.ComboBox();
+            this.btnPickColor = new System.Windows.Forms.Button();
+            this.lblSelectedColor = new System.Windows.Forms.Label();
+            this.lblCountry = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.dtpBirthdate = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.picStudent = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picStudent)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(172, 131);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(121, 27);
+            this.txtName.TabIndex = 0;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(172, 184);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(121, 27);
+            this.txtEmail.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(172, 239);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(121, 27);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.BackColor = System.Drawing.Color.White;
+            this.lblName.Location = new System.Drawing.Point(180, 107);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(36, 19);
+            this.lblName.TabIndex = 3;
+            this.lblName.Text = "الاسم";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.BackColor = System.Drawing.Color.White;
+            this.lblPassword.Location = new System.Drawing.Point(174, 217);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(55, 19);
+            this.lblPassword.TabIndex = 4;
+            this.lblPassword.Text = "كلمة السر";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.BackColor = System.Drawing.Color.White;
+            this.lblEmail.Location = new System.Drawing.Point(175, 162);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(41, 19);
+            this.lblEmail.TabIndex = 5;
+            this.lblEmail.Text = "الإيميل";
+            // 
+            // lblBirthdate
+            // 
+            this.lblBirthdate.AutoSize = true;
+            this.lblBirthdate.BackColor = System.Drawing.Color.White;
+            this.lblBirthdate.Location = new System.Drawing.Point(319, 109);
+            this.lblBirthdate.Name = "lblBirthdate";
+            this.lblBirthdate.Size = new System.Drawing.Size(62, 19);
+            this.lblBirthdate.TabIndex = 12;
+            this.lblBirthdate.Text = "ادخل العمر";
+            // 
+            // lblGender
+            // 
+            this.lblGender.AutoSize = true;
+            this.lblGender.BackColor = System.Drawing.Color.White;
+            this.lblGender.Location = new System.Drawing.Point(325, 175);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(71, 19);
+            this.lblGender.TabIndex = 13;
+            this.lblGender.Text = "اختير الجنس";
+            // 
+            // rdoFemale
+            // 
+            this.rdoFemale.AutoSize = true;
+            this.rdoFemale.BackColor = System.Drawing.Color.White;
+            this.rdoFemale.Location = new System.Drawing.Point(323, 197);
+            this.rdoFemale.Name = "rdoFemale";
+            this.rdoFemale.Size = new System.Drawing.Size(48, 23);
+            this.rdoFemale.TabIndex = 14;
+            this.rdoFemale.TabStop = true;
+            this.rdoFemale.Text = "أنثى";
+            this.rdoFemale.UseVisualStyleBackColor = false;
+            // 
+            // rdoMale
+            // 
+            this.rdoMale.AutoSize = true;
+            this.rdoMale.BackColor = System.Drawing.Color.White;
+            this.rdoMale.Location = new System.Drawing.Point(380, 197);
+            this.rdoMale.Name = "rdoMale";
+            this.rdoMale.Size = new System.Drawing.Size(47, 23);
+            this.rdoMale.TabIndex = 15;
+            this.rdoMale.TabStop = true;
+            this.rdoMale.Text = "ذكر";
+            this.rdoMale.UseVisualStyleBackColor = false;
+            // 
+            // cmbCountry
+            // 
+            this.cmbCountry.BackColor = System.Drawing.Color.White;
+            this.cmbCountry.FormattingEnabled = true;
+            this.cmbCountry.Items.AddRange(new object[] {
+            "اليمن",
+            "افغانستان"});
+            this.cmbCountry.Location = new System.Drawing.Point(172, 299);
+            this.cmbCountry.Name = "cmbCountry";
+            this.cmbCountry.Size = new System.Drawing.Size(121, 27);
+            this.cmbCountry.TabIndex = 17;
+            this.cmbCountry.Text = "اختر الدولة";
+            // 
+            // btnPickColor
+            // 
+            this.btnPickColor.BackColor = System.Drawing.Color.Coral;
+            this.btnPickColor.ForeColor = System.Drawing.Color.White;
+            this.btnPickColor.Location = new System.Drawing.Point(172, 341);
+            this.btnPickColor.Name = "btnPickColor";
+            this.btnPickColor.Size = new System.Drawing.Size(121, 29);
+            this.btnPickColor.TabIndex = 18;
+            this.btnPickColor.Text = "اختر اللون";
+            this.btnPickColor.UseVisualStyleBackColor = false;
+            this.btnPickColor.Click += new System.EventHandler(this.btnPickColor_Click);
+            // 
+            // lblSelectedColor
+            // 
+            this.lblSelectedColor.AutoSize = true;
+            this.lblSelectedColor.BackColor = System.Drawing.Color.White;
+            this.lblSelectedColor.Location = new System.Drawing.Point(184, 376);
+            this.lblSelectedColor.Name = "lblSelectedColor";
+            this.lblSelectedColor.Size = new System.Drawing.Size(95, 19);
+            this.lblSelectedColor.TabIndex = 19;
+            this.lblSelectedColor.Text = "لم يتم اختيار اللون";
+            // 
+            // lblCountry
+            // 
+            this.lblCountry.AutoSize = true;
+            this.lblCountry.BackColor = System.Drawing.Color.White;
+            this.lblCountry.Location = new System.Drawing.Point(170, 277);
+            this.lblCountry.Name = "lblCountry";
+            this.lblCountry.Size = new System.Drawing.Size(84, 19);
+            this.lblCountry.TabIndex = 20;
+            this.lblCountry.Text = "اختر اسم الدوله";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.BackColor = System.Drawing.Color.Coral;
+            this.lblResult.Font = new System.Drawing.Font("The Year of Handicrafts", 20F);
+            this.lblResult.ForeColor = System.Drawing.Color.White;
+            this.lblResult.Location = new System.Drawing.Point(580, 327);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(112, 50);
+            this.lblResult.TabIndex = 21;
+            this.lblResult.Text = "النتيجة";
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.Coral;
+            this.btnReset.ForeColor = System.Drawing.Color.White;
+            this.btnReset.Location = new System.Drawing.Point(580, 168);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(112, 32);
+            this.btnReset.TabIndex = 22;
+            this.btnReset.Text = "اعاده التعيين";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Coral;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(323, 226);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 27);
+            this.button2.TabIndex = 23;
+            this.button2.Text = "رفع الصور";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.Color.Coral;
+            this.btnSubmit.ForeColor = System.Drawing.Color.White;
+            this.btnSubmit.Location = new System.Drawing.Point(580, 259);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(112, 36);
+            this.btnSubmit.TabIndex = 24;
+            this.btnSubmit.Text = "تسجيل";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.Coral;
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(580, 131);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(112, 27);
+            this.btnSave.TabIndex = 25;
+            this.btnSave.Text = "حفظ البيانات";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.BackColor = System.Drawing.Color.Coral;
+            this.btnLoad.ForeColor = System.Drawing.Color.White;
+            this.btnLoad.Location = new System.Drawing.Point(580, 211);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(112, 30);
+            this.btnLoad.TabIndex = 26;
+            this.btnLoad.Text = "تحميل البيانات";
+            this.btnLoad.UseVisualStyleBackColor = false;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // dtpBirthdate
+            // 
+            this.dtpBirthdate.Location = new System.Drawing.Point(323, 131);
+            this.dtpBirthdate.Name = "dtpBirthdate";
+            this.dtpBirthdate.Size = new System.Drawing.Size(200, 27);
+            this.dtpBirthdate.TabIndex = 28;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(76, 59);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(690, 407);
+            this.groupBox1.TabIndex = 29;
+            this.groupBox1.TabStop = false;
+            // 
+            // picStudent
+            // 
+            this.picStudent.BackColor = System.Drawing.Color.White;
+            this.picStudent.Image = ((System.Drawing.Image)(resources.GetObject("picStudent.Image")));
+            this.picStudent.Location = new System.Drawing.Point(323, 259);
+            this.picStudent.Name = "picStudent";
+            this.picStudent.Size = new System.Drawing.Size(200, 136);
+            this.picStudent.TabIndex = 27;
+            this.picStudent.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOrange;
+            this.ClientSize = new System.Drawing.Size(864, 534);
+            this.Controls.Add(this.dtpBirthdate);
+            this.Controls.Add(this.picStudent);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblCountry);
+            this.Controls.Add(this.lblSelectedColor);
+            this.Controls.Add(this.btnPickColor);
+            this.Controls.Add(this.cmbCountry);
+            this.Controls.Add(this.rdoMale);
+            this.Controls.Add(this.rdoFemale);
+            this.Controls.Add(this.lblGender);
+            this.Controls.Add(this.lblBirthdate);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("The Year of Handicrafts", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picStudent)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblBirthdate;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.RadioButton rdoFemale;
+        private System.Windows.Forms.RadioButton rdoMale;
+        private System.Windows.Forms.ComboBox cmbCountry;
+        private System.Windows.Forms.Button btnPickColor;
+        private System.Windows.Forms.Label lblSelectedColor;
+        private System.Windows.Forms.Label lblCountry;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.PictureBox picStudent;
+        private System.Windows.Forms.DateTimePicker dtpBirthdate;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ task1/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Text.RegularExpressions; // ضروري لفحص الإيميل
+using System.IO; // ضروري للحفظ والتحميل
+
+namespace task1
+{
+    public partial class Form1 : Form
+    {
+        // ------------------------------------------
+        //  التصليح الأول: هنا كان اسم الدالة غلط
+        // ------------------------------------------
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // --- دالة زر التسجيل (مع التحقق) ---
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            // (بقية الكود زي ما هو) ...
+            if (string.IsNullOrWhiteSpace(txtName.Text))
+            {
+                MessageBox.Show("الاسم مطلوب!", "خطأ في التحقق",
+                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtName.Focus();
+                return;
+            }
+
+            if (!Regex.IsMatch(txtEmail.Text, @"^[^@\s]+@[^@\s]+\.[^@\s]+$"))
+            {
+                MessageBox.Show("ادخل بريد إلكتروني صحيح!", "خطأ في التحقق",
+                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtEmail.Focus();
+                return;
+            }
+
+            if (txtPassword.Text.Length < 6)
+            {
+                MessageBox.Show("كلمة السر لازم تكون 6 أحرف أو أكثر!",
+                "خطأ في التحقق", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtPassword.Focus();
+                return;
+            }
+
+            if (!rdoMale.Checked && !rdoFemale.Checked)
+            {
+                MessageBox.Show("الرجاء اختيار الجنس!", "خطأ في التحقق",
+                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (cmbCountry.SelectedItem == null)
+            {
+                MessageBox.Show("الرجاء اختيار الدولة!", "خطأ في التحقق",
+                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                cmbCountry.Focus();
+                return;
+            }
+
+            if (lblSelectedColor.Text == "لم يتم اختيار اللون")
+            {
+                MessageBox.Show("الرجاء اختيار اللون المفضل!",
+                "خطأ في التحقق", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // All validations passed
+            string name = txtName.Text;
+            string email = txtEmail.Text;
+            string gender = rdoMale.Checked ? "ذكر" : "أنثى";
+            string birthdate = dtpBirthdate.Value.ToShortDateString();
+            string country = cmbCountry.SelectedItem.ToString();
+
+            // ------------------------------------------
+            //  التصليح الثاني: هنا كان النص الإنجليزي غلط
+            // ------------------------------------------
+            string color = lblSelectedColor.Text.Replace("اللون المختار: ", "");
+
+            // Displaying result
+            lblResult.Text = $"الاسم: {name}\nالإيميل: {email}\nالجنس: {gender}\n" +
+                             $"تاريخ الميلاد: {birthdate}\nالدولة: {country}\nاللون المفضل: {color}";
+        }
+
+        // --- دالة زر اختيار اللون ---
+        private void btnPickColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                lblSelectedColor.Text = $"اللون المختار: {colorDialog.Color.Name}";
+            }
+        }
+
+        // --- دالة زر إعادة التعيين (النسخة النهائية) ---
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            // (الكود سليم 100%)
+            txtName.Text = "";
+            txtEmail.Text = "";
+            txtPassword.Text = "";
+            rdoMale.Checked = false;
+            rdoFemale.Checked = false;
+            cmbCountry.SelectedIndex = -1;
+            dtpBirthdate.Value = DateTime.Now;
+            lblSelectedColor.Text = "لم يتم اختيار اللون";
+            lblResult.Text = "النتيجة";
+            picStudent.Image = null;
+            txtName.Focus();
+        }
+
+        // --- دالة زر رفع الصورة ---
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            // (الكود سليم 100%)
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "ملفات الصور|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                picStudent.Image = Image.FromFile(openFileDialog.FileName);
+            }
+        }
+
+        // --- دالة زر حفظ البيانات ---
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            // (الكود سليم 100%)
+            if (string.IsNullOrWhiteSpace(txtName.Text) || string.IsNullOrWhiteSpace(txtEmail.Text))
+            {
+                MessageBox.Show("الرجاء تعبئة الاسم والإيميل قبل الحفظ!", "خطأ في التحقق", MessageBoxButtons.OK,
+                MessageBoxIcon.Warning);
+                return;
+            }
+
+            string gender = rdoMale.Checked ? "Male" : (rdoFemale.Checked ? "Female" : "N/A");
+            string data = $"{txtName.Text}\n" +          // Name
+                          $"{txtEmail.Text}\n" +         // Email
+                          $"{txtPassword.Text}\n" +      // Password
+                          $"{gender}\n" +                // Gender
+                          $"{dtpBirthdate.Value.ToShortDateString()}\n" + // Birthdate
+                          $"{cmbCountry.SelectedItem?.ToString()}\n" + // Country
+                          $"{lblSelectedColor.Text.Replace("اللون المختار: ", "").Replace("لم يتم اختيار اللون", "")}\n" + // Favorite Color
+                          $"{(picStudent.Image != null ? "student_picture.jpg" : "NoImage")}\n"; // Image Path
+
+            File.WriteAllText("student_data.txt", data);
+
+            if (picStudent.Image != null)
+            {
+                picStudent.Image.Save("student_picture.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+            }
+
+            MessageBox.Show("تم حفظ البيانات بنجاح!", "نجاح",
+            MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // --- دالة زر تحميل البيانات ---
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            // (الكود سليم 100%)
+            if (!File.Exists("student_data.txt"))
+            {
+                MessageBox.Show("لا يوجد ملف بيانات محفوظ!", "خطأ",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string[] lines = File.ReadAllLines("student_data.txt");
+
+            if (lines.Length < 8)
+            {
+                MessageBox.Show("ملف البيانات تالف أو غير مكتمل!", "خطأ",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            txtName.Text = lines[0];
+            txtEmail.Text = lines[1];
+            txtPassword.Text = lines[2];
+
+            if (lines[3] == "Male") rdoMale.Checked = true;
+            else if (lines[3] == "Female") rdoFemale.Checked = true;
+
+            if (DateTime.TryParse(lines[4], out DateTime birthDate))
+            {
+                dtpBirthdate.Value = birthDate;
+            }
+
+            cmbCountry.SelectedItem = lines[5];
+
+            if (!string.IsNullOrEmpty(lines[6]))
+            {
+                lblSelectedColor.Text = "اللون المختار: " + lines[6];
+            }
+            else
+            {
+                lblSelectedColor.Text = "لم يتم اختيار اللون";
+            }
+
+            if (File.Exists("student_picture.jpg") && lines[7] == "student_picture.jpg")
+            {
+                using (var bmpTemp = new Bitmap("student_picture.jpg"))
+                {
+                    picStudent.Image = new Bitmap(bmpTemp);
+                }
+            }
+            else
+            {
+                picStudent.Image = null;
+            }
+
+            MessageBox.Show("تم تحميل البيانات بنجاح!", "نجاح",
+            MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
